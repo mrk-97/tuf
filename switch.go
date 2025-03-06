9e2d65467f22b46b26c74f7fb53d2a9a49026e41
Manoj Reddy <manoj.reddy@beepkart.com>
2025-03-06 21:46:20 +0530

    added basic switch

---
+++ switch.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func dayOfWeek(day int) string {
+	switch day {
+	case 1:
+		return "Monday"
+	case 2:
+		return "Tuesday"
+	case 3:
+		return "Wednesday"
+	case 4:
+		return "Thursday"
+	case 5:
+		return "Friday"
+	case 6:
+		return "Saturday"
+	case 7:
+		return "Sunday"
+	default:
+		return "Invalid Day"
+	}
+
+}
+
+func main() {
+	fmt.Println(dayOfWeek(2))
+}
