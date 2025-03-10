f1b0089a8a02d09efb6c9eafb8ca47d9a53d1544
Manoj Reddy <manojkatta97@gmail.com>
2025-03-10 07:40:01 +0530

    day04 commit

--- day-04/practice-04.go
+++ day-04/practice-04.go
@@ -11,6 +11,36 @@ func printStarPattern(n int) {
 	}
 }
 
+func printDiagonalPattern(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+func printDiagonalNumberPattern(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			fmt.Print(j)
+		}
+		fmt.Println(i)
+	}
+}
+func printDiagonalNumberPattern11(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			fmt.Print(j)
+		}
+		fmt.Println(i + i)
+	}
+}
+
 func main() {
 	printStarPattern(5)
+	printDiagonalPattern(5)
+	printDiagonalNumberPattern(5)
+	printDiagonalNumberPattern11(4)
+
 }
